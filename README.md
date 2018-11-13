c0249d3e2bbc953d0de8b356de5c447d90fcf5d1
tadashi-aikawa <syou.maman@gmail.com>
2018-11-13 22:17:13 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# owlcode
+
+TODO:
+
+## Features
+
+TODO:
+
+## Extension Settings
+
+TODO:
+
+Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+
+For example:
+
+This extension contributes the following settings:
+
+* `myExtension.enable`: enable/disable this extension
+* `myExtension.thing`: set to `blah` to do something
+
+## Release Notes
+
+TODO:
+
+### 1.0.0
+
+Initial release of ...
+
+TODO:
---
+++ src/extension.ts
@@ -0,0 +1,46 @@
+'use strict';
+import * as vscode from 'vscode';
+
+const sum = (x: number, y: number): number => x + y;
+
+// Shift_JIS: 0x0 ～ 0x80, 0xa0 , 0xa1 ～ 0xdf , 0xfd ～ 0xff
+// Unicode : 0x0 ～ 0x80, 0xf8f0, 0xff61 ～ 0xff9f, 0xf8f1 ～ 0xf8f3
+const getBytes = (c: number | undefined): number =>
+    c === undefined ? 0 :
+        (c >= 0x0 && c < 0x81) || (c === 0xf8f0) || (c >= 0xff61 && c < 0xffa0) || (c >= 0xf8f1 && c < 0xf8f4) ?
+            1 : 2;
+
+const countLength = (str: string): number => str.split('')
+        .map(x => getBytes(x.codePointAt(0)))
+        .reduce(sum);
+
+const setHeader = (symbol: string) =>
+    new Promise((resolve, reject) => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+
+        // 現在行の文字列数を取得
+        const titleLength = countLength(
+            editor.document.lineAt(editor.selection.active.line).text
+        );
+
+        editor.edit(editBuilder => {
+            const currentPos = editor.selection.active;
+            editBuilder.insert(new vscode.Position(currentPos.line, 999), `\n${symbol.repeat(titleLength)}`)
+            resolve();
+        });
+    });
+
+export function activate(context: vscode.ExtensionContext) {
+    const register = vscode.commands.registerCommand;
+
+    context.subscriptions.push(
+        register('extension.headerLV1', async () => await setHeader("=")),
+        register('extension.headerLV2', async () => await setHeader("-")),
+    );
+}
+
+export function deactivate() {
+}
