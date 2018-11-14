db4352f7e285183c128ff1176ac3d15eb34ab5fb
tadashi-aikawa <syou.maman@gmail.com>
2018-11-14 17:14:30 +0900

    Revert ":hammer: Not install devdependencies"
    This reverts commit 81a73098ba12f717b7c626769886470f1dddd1f3.

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
 git clone https://github.com/tadashi-aikawa/owlcode.git %HOMEPATH%\.vscode\extensions\owlcode ^
   && cd %HOMEPATH%\.vscode\extensions\owlcode ^
-  && npm i --production ^
+  && npm i ^
   && npm run compile
