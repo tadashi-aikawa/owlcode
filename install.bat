81a73098ba12f717b7c626769886470f1dddd1f3
tadashi-aikawa <syou.maman@gmail.com>
2018-11-14 17:10:42 +0900

    :hammer: Not install devdependencies

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
 git clone https://github.com/tadashi-aikawa/owlcode.git %HOMEPATH%\.vscode\extensions\owlcode ^
   && cd %HOMEPATH%\.vscode\extensions\owlcode ^
-  && npm i ^
+  && npm i --production ^
   && npm run compile
